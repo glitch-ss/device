c9e55776e765490c1ff85023dee5f5c35751d98d
glitch-ss <547076180@qq.com>
2021-05-31 15:18:00 +0800

    Device init

---
+++ src/pojo/CPU.java
@@ -0,0 +1,15 @@
+package pojo;
+
+public class CPU {
+	private String name;
+	private String partNumber;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPartNumber() {
+		return partNumber;
+	}
+}
