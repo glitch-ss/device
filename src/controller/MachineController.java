db44f74584d7420a2ce73d1f78d0f55c42f2aed4
glitch-ss <547076180@qq.com>
2021-07-06 15:30:50 +0800

    machine config completed

--- src/controller/MachineController.java
+++ src/controller/MachineController.java
@@ -19,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import dao.PCIeCategoryDAO;
+import pojo.PCIeCategory;
+
 @Controller
 public class MachineController {
 	@RequestMapping("/machine")
@@ -33,6 +36,7 @@ public class MachineController {
 		String label = machine.substring(0, 5);
 		String page_file = "machine-" + label;
 		ModelAndView mav = new ModelAndView(page_file);
+		PCIeCategoryDAO pd = new PCIeCategoryDAO();
 		
 		
 		
@@ -41,12 +45,18 @@ public class MachineController {
 
 		String file1 = FileUtils.readFileToString(file);
 		JSONObject machineJson = JSON.parseObject(file1);
-		JSONObject pci = machineJson.getJSONObject("pci");
+		JSONObject pci = machineJson.getJSONObject("pcie");
 		for (Map.Entry entry : pci.entrySet()) {
 			String slot = String.valueOf(entry.getKey());
 			JSONObject device =  (JSONObject) JSONObject.toJSON(entry.getValue());
-			
+			System.out.println("test");
+			System.out.println(device);
+			String partnumber = String.valueOf(device.get("partnumber"));
+			PCIeCategory pciitem = pd.get(partnumber);
+			System.out.println(pciitem);
+			device.put("nickname", pciitem.nickname);
 		}
+		mav.addObject("name", machine);
 		mav.addObject("machine", machineJson);
 		return mav;
 	}
